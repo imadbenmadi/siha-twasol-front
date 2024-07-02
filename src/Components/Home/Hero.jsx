e66957f1815ca65f3dab056d9c755324636b2cc0
imadbenmadi <imadbenmadi891@gmail.com>
2024-07-02 15:34:53 +0100

    feat: Update Hero component layout and styles

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -5,9 +5,10 @@ function Hero() {
     const Navigate = useNavigate();
     return (
         <div>
-            <div className=" flex justify-center  gap-6 mt-24">
+            <div className=" flex flex-col md:flex-row justify-center items-center md:items-start
+              md:gap-6 md:mt-24 mt-20">
                 <img src={Hero_image} className=" w-[320px] shrink-0" alt="" />
-                <div className="h-full pt-12 max-w-[350px]">
+                <div className="h-full md:pt-12 max-w-[350px]">
                     <div className=" font-semibold text-2xl  ">
                         تواصل مع مقدم الرعاية الصحية الخاص بك بسلاسة
                     </div>
