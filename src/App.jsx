68b2eb59e533c541974c5bd2c5c0e16786068aac
imadbenmadi <imadbenmadi891@gmail.com>
2024-07-02 15:08:49 +0100

    chnagin the font

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,8 @@ function App() {
         const fetch_fonts = () => {
             return new Promise((resolve, reject) => {
                 const fontURL =
-                    "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap";
+                    // "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap";
+                    "https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap";
                 const loadFont = (url) => {
                     return new Promise((resolve, reject) => {
                         const link = document.createElement("link");
@@ -66,7 +67,7 @@ function App() {
                         };
                         document.head.appendChild(link);
                         document.getElementById("root").style.fontFamily =
-                            "Poppins";
+                            "cairo";
                     });
                 };
 
