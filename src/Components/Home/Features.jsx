31535602b46e44db3ca6e23e34fc69d865205cc1
imadbenmadi <imadbenmadi891@gmail.com>
2024-07-02 17:30:03 +0100

    refactor: Update Features component layout and styles

--- src/Components/Home/Features.jsx
+++ src/Components/Home/Features.jsx
@@ -13,10 +13,10 @@ function Features() {
                 </div>
             </div>
             <div
-                className=" flex flex-col md:flex-row items-center md:justify-center gap-12  md:overflow-x-auto overflow-hidden
+                className=" flex flex-col md:flex-row items-center lg:justify-center gap-12  md:overflow-x-auto overflow-hidden
             custom-overflow px-12 py-12"
             >
-                <div className=" w-[300px] h-[250px] shrink-0 border shadow-[#2a77f147] shadow-xl  rounded-xl py-4">
+                <div className=" w-[300px] md:h-[250px] shrink-0 border shadow-[#2a77f147] shadow-xl  rounded-xl py-4">
                     <div>
                         <img
                             src={Feature1_image}
@@ -25,16 +25,16 @@ function Features() {
                         />
                     </div>
                     <div className=" text-center">
-                        <div className=" text-xl font-semibold mb-4">
+                        <div className=" text-lg md:text-xl font-semibold mb-4">
                             إشعارات الأدوية واللقاحات
                         </div>
-                        <div>
+                        <div className=" text-sm md:text-base">
                             احصل على إشعارات بشأن الحملات الصحية وأيام التوعية
                             والخدمات الطبية المجانية.
                         </div>
                     </div>
                 </div>{" "}
-                <div className=" w-[300px] h-[250px] shrink-0 border shadow-[#2a77f147] shadow-xl  rounded-xl py-4">
+                <div className=" w-[300px]  md:h-[250px] shrink-0 border shadow-[#2a77f147] shadow-xl  rounded-xl py-4">
                     <div>
                         <img
                             src={Feature2_image}
@@ -43,16 +43,16 @@ function Features() {
                         />
                     </div>
                     <div className=" text-center">
-                        <div className=" text-xl font-semibold mb-4">
+                        <div className=" text-lg md:text-xl font-semibold mb-4">
                             إشعارات الأدوية واللقاحات
                         </div>
-                        <div>
+                        <div className=" text-sm md:text-base">
                             احصل على إشعارات بشأن الحملات الصحية وأيام التوعية
                             والخدمات الطبية المجانية.
                         </div>
                     </div>
                 </div>{" "}
-                <div className=" w-[300px] h-[250px] shrink-0 border shadow-[#2a77f147] shadow-xl  rounded-xl py-4">
+                <div className=" w-[300px]  md:h-[250px] shrink-0 border shadow-[#2a77f147] shadow-xl  rounded-xl py-4">
                     <div>
                         <img
                             src={Feature3_image}
@@ -61,10 +61,10 @@ function Features() {
                         />
                     </div>
                     <div className=" text-center">
-                        <div className=" text-xl font-semibold mb-4">
+                        <div className=" text-lg md:text-xl font-semibold mb-4">
                             إشعارات الأدوية واللقاحات
                         </div>
-                        <div>
+                        <div className=" text-sm md:text-base">
                             احصل على إشعارات بشأن الحملات الصحية وأيام التوعية
                             والخدمات الطبية المجانية.
                         </div>
