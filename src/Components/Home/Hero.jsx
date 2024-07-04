9fe4502989bfd1d56044bef97c77f0456672b9ad
imadbenmadi <imadbenmadi891@gmail.com>
2024-07-04 15:59:05 +0100

    refactor: Update Hero component layout and styles

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -5,16 +5,19 @@ function Hero() {
     const Navigate = useNavigate();
     return (
         <div>
-            <div className=" flex flex-col md:flex-row justify-center items-center md:items-start
-              md:gap-12 md:mt-40 mt-20">
+            <div
+                className=" flex flex-col md:flex-row justify-center items-center md:items-start
+              md:gap-12 md:mt-40 mt-20"
+            >
                 <img src={Hero_image} className=" w-[320px] shrink-0" alt="" />
-                <div className="h-full md:pt-12 max-w-[350px]">
+                <div className="h-full md:pt-12 max-w-[380px] px-4">
                     <div className=" font-semibold text-2xl  ">
-                        تواصل مع مقدم الرعاية الصحية الخاص بك بسلاسة
+                        كل ما تحتاجه من خدمات التواصل الصحي ستجده معنا في هذه
+                        المنصة
                     </div>
-                    <div className=" text-sm pt-2">
-                        يمكنك تلقي التحديثات الصحية والبقاء على اطلاع على جميع
-                        المؤسسات الاستشفائية
+                    <div className=" text-sm pt-4">
+                        SCS انضم إلى منصة <br />و استمتع بتجربة تواصل صحي
+                        جديدة و مميزة
                     </div>
                     <div
                         onClick={() => {
