ca34a7df3aef197ab6b6a3d01681fcfd8c35fe6f
imadbenmadi <imadbenmadi891@gmail.com>
2024-07-06 15:19:43 +0100

    refactor: Update NavBar component with Arabic translations and fix class name typo

--- src/Components/Home/NavBar/Mobile_Nav_Items.jsx
+++ src/Components/Home/NavBar/Mobile_Nav_Items.jsx
@@ -9,7 +9,7 @@ function Mobile_Nav_Items({ MobileNav_Open, Toogle_Menu_Bar }) {
                         ? " translate-x-[0vw]"
                         : " translate-x-[200vh] "
                 } absolute   transition-transform duration-300 select-none w-[100vw]
-                  z-50    text-black   bg-white `}
+                  z-50    text-black_text   bg-white `}
             >
                 <div className="  h-screen text-xl  pt-8 overflow-y-auto ">
                     <div className=" flex flex-col justify-start items-center h-[80%]  ">
