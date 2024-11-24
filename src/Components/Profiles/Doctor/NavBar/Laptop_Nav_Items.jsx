0f18ca503fc4cf7a800c54efd9059b8c4f590244
imadbenmadi <imadbenmadi891@gmail.com>
2024-11-24 13:29:43 +0100

    Add notification feature to Mobile_Nav component; implement notification display, delete functionality, and toggle behavior

--- src/Components/Profiles/Doctor/NavBar/Laptop_Nav_Items.jsx
+++ src/Components/Profiles/Doctor/NavBar/Laptop_Nav_Items.jsx
@@ -6,6 +6,7 @@ import { TbLogout2 } from "react-icons/tb";
 import { FiUser } from "react-icons/fi";
 import { useAppContext } from "../../../../AppContext";
 import { useNavigate } from "react-router-dom";
+import notification_icon from "../../../../../public/Notification.png";
 
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
@@ -20,7 +21,24 @@ function Laptop_Nav_Items({ Active_nav, handleLogout, LogoutClicked }) {
         setProfileClicked(!ProfileClicked);
     };
     const [open_Notifications, setopen_Notifications] = useState(false);
+    const toogleopen_Notifications = () => {
+        setProfileClicked(false);
+        setopen_Notifications(!open_Notifications);
+    };
+    const Delete_Notification = (id) => {
+        const newNotifications = Notifications.filter(
+            (notification) => notification.id !== id
+        );
+        set_Notifications(newNotifications);
+        axios.delete(
+            `http://localhost:3000/Teachers/${user?.id}/Notifications/${id}`,
 
+            {
+                withCredentials: true,
+                validateStatus: () => true,
+            }
+        );
+    };
     return (
         <div className="hidden  md:flex  items-center justify-between mx-2 lg:mx-12  md:text-md lg:text-lg  font-[500] text-black_text h-full p-2 ">
             <div>
@@ -85,110 +103,131 @@ function Laptop_Nav_Items({ Active_nav, handleLogout, LogoutClicked }) {
                 </div>
             </div>
             <div className=" flex items center justify-center gap-5">
-                <div className="flex items-center justify-center gap-6 "></div>
-                <div className=" relative">
-                    {user?.profile_pic_link ? (
-                        <img
-                            src={`http://localhost:3000/${user.profile_pic_link}`}
-                            onError={(e) => {
-                                e.target.onerror = null;
-                                e.target.src = user_default;
-                            }}
-                            alt="pic"
-                            className=" w-8 cursor-pointer"
-                            onClick={toogleProfile}
-                        />
-                    ) : (
-                        <img
-                            src={user_default}
-                            alt="pic"
-                            className=" w-8 cursor-pointer"
-                            onClick={toogleProfile}
-                        />
-                    )}
-
-                    {ProfileClicked ? (
-                        <div
-                            className="absolute top-10 right-0 bg-white shadow border  
-                    rounded-lg p-2 w-40 z-50 flex items-center  flex-col gap-3"
-                        >
-                            <div className="">
-                                {LogoutClicked ? (
-                                    <div className="w-full ">
-                                        <span className="small-loader font-bold  w-full m-auto"></span>
-                                    </div>
-                                ) : (
-                                    <div
-                                        className="cursor-pointer w-full 
-                                    flex items-center gap-3 text-red-500"
-                                        onClick={() => {
-                                            handleLogout();
-                                        }}
-                                    >
-                                        <TbLogout2 className="  text-xl" />
-                                        Logout
-                                    </div>
+                <div className="flex items-center justify-center gap-6 relative ">
+                    <div className="flex items-center justify-center gap-6 ">
+                        <div className="flex items-center justify-center gap-6 relative">
+                            <div className="relative">
+                                {Notifications?.length > 0 && (
+                                    <div className=" w-2 h-2 rounded-full bg-red-500 absolute top-0 right-0 "></div>
                                 )}
+                                <img
+                                    src={notification_icon}
+                                    alt=""
+                                    className=" cursor-pointer shrink-0 w-full"
+                                    onClick={toogleopen_Notifications}
+                                />
                             </div>
                         </div>
-                    ) : null}
-                    {open_Notifications ? (
-                        <div
-                            className="absolute top-10 right-0 bg-white shadow border  
+                    </div>
+                    <div className=" relative">
+                        {user?.profile_pic_link ? (
+                            <img
+                                src={`http://localhost:3000/${user.profile_pic_link}`}
+                                onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src = user_default;
+                                }}
+                                alt="pic"
+                                className=" w-8 cursor-pointer rounded-full object-cover"
+                                onClick={toogleProfile}
+                            />
+                        ) : (
+                            <img
+                                src={user_default}
+                                alt="pic"
+                                className=" w-8 cursor-pointer"
+                                onClick={toogleProfile}
+                            />
+                        )}
+                    </div>{" "}
+                    <div className=" ">
+                        {ProfileClicked ? (
+                            <div
+                                className="absolute top-10 right-0 bg-white shadow border  
+                    rounded-lg p-2 w-40 z-50 flex items-center  flex-col gap-3"
+                            >
+                                <div className="">
+                                    {LogoutClicked ? (
+                                        <div className="w-full ">
+                                            <span className="small-loader font-bold  w-full m-auto"></span>
+                                        </div>
+                                    ) : (
+                                        <div
+                                            className="cursor-pointer w-full 
+                                    flex items-center gap-3 text-red-500"
+                                            onClick={() => {
+                                                handleLogout();
+                                            }}
+                                        >
+                                            <TbLogout2 className="  text-xl" />
+                                            Logout
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        ) : null}
+                        {open_Notifications ? (
+                            <div
+                                className="absolute top-10 right-0 bg-white shadow border  
                              rounded-lg   z-50   min-w-[500px] h-[calc(100vh-100px)] overflow-y-auto
                              custom-overflow
                              "
-                        >
-                            <div className=" text-gray_v py-2 px-7 text-md font-semibold border-b">
-                                Notifications
-                            </div>
-                            <div>
-                                {Notifications?.length > 0 ? (
-                                    Notifications.map((notification) => (
-                                        <div
-                                            onClick={() => {
-                                                Delete_Notification(
-                                                    notification.id
-                                                );
-                                                Navigate(notification.link);
-                                                setopen_Notifications(false);
-                                                setProfileClicked(false);
-                                            }}
-                                            key={notification.id}
-                                            className="flex items-center gap-3 py-1 px-3 border-b cursor-pointer hover:bg-gray-100"
-                                        >
-                                            <div className=" flex gap-2 ">
-                                                <IoMdNotificationsOutline className="shrink-0 mt-2" />
-                                                <div>
-                                                    <div className="text-black_text font-semibold">
-                                                        {notification?.title}
-                                                    </div>
-                                                    <div className="text-gray_v text-sm">
-                                                        {notification?.text}
-                                                    </div>
-                                                    <div className="text-gray_v text-xs pt-1">
-                                                        {/* {new Date(
+                            >
+                                <div className=" text-gray_v py-2 px-7 text-md font-semibold border-b">
+                                    Notifications
+                                </div>
+                                <div>
+                                    {Notifications?.length > 0 ? (
+                                        Notifications.map((notification) => (
+                                            <div
+                                                onClick={() => {
+                                                    Delete_Notification(
+                                                        notification.id
+                                                    );
+                                                    Navigate(notification.link);
+                                                    setopen_Notifications(
+                                                        false
+                                                    );
+                                                    setProfileClicked(false);
+                                                }}
+                                                key={notification.id}
+                                                className="flex items-center gap-3 py-1 px-3 border-b cursor-pointer hover:bg-gray-100"
+                                            >
+                                                <div className=" flex gap-2 ">
+                                                    <IoMdNotificationsOutline className="shrink-0 mt-2" />
+                                                    <div>
+                                                        <div className="text-black_text font-semibold">
+                                                            {
+                                                                notification?.title
+                                                            }
+                                                        </div>
+                                                        <div className="text-gray_v text-sm">
+                                                            {notification?.text}
+                                                        </div>
+                                                        <div className="text-gray_v text-xs pt-1">
+                                                            {/* {new Date(
                                                             notification?.createdAt
                                                         ).toLocaleDateString()} */}
-                                                        {dayjs(
-                                                            notification?.createdAt
-                                                        ).format(
-                                                            "DD MMMM YYYY"
-                                                        )}
+                                                            {dayjs(
+                                                                notification?.createdAt
+                                                            ).format(
+                                                                "DD MMMM YYYY"
+                                                            )}
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
+                                        ))
+                                    ) : (
+                                        <div className="text-center text-gray_v pt-6 flex-gap-2 ">
+                                            No Notifications
                                         </div>
-                                    ))
-                                ) : (
-                                    <div className="text-center text-gray_v pt-6 flex-gap-2 ">
-                                        No Notifications
-                                    </div>
-                                )}
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    ) : null}
-                </div>{" "}
+                        ) : null}
+                    </div>
+                </div>
             </div>
         </div>
     );
