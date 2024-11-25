fd73fc593fc46d697f0adfff2e190ce000091f50
imadbenmadi <imadbenmadi891@gmail.com>
2024-11-25 09:45:19 +0100

    Refactor Malads_List component to improve filtering and display patient information

--- src/Components/Profiles/Doctor/Malads/Malads_List.jsx
+++ src/Components/Profiles/Doctor/Malads/Malads_List.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useAppContext } from "../../../../AppContext";
 import axios from "axios";
 import { Link, useLocation } from "react-router-dom";
-
+import { IoSearch } from "react-icons/io5";
 function List() {
     const { user } = useAppContext();
 
@@ -11,13 +11,10 @@ function List() {
     const [malads, setMalads] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [filteredCompanies, setFilteredCompanies] = useState([]);
+    const [filteredMalads, setFilteredMalads] = useState([]);
 
     // Filters
     const [searchQuery, setSearchQuery] = useState("");
-    const [typeFilter, setTypeFilter] = useState("");
-    const [locationFilter, setLocationFilter] = useState("");
-    const [locations, setLocations] = useState([]);
 
     useEffect(() => {
         const fetchMalads = async () => {
@@ -26,26 +23,13 @@ function List() {
                     `http://localhost:3000/Doctors/${user.id}/Malads`,
                     { withCredentials: true, validateStatus: () => true }
                 );
-
-                setMalads(response.data);
-                setLoading(false);
-
-                if (Array.isArray(response.data)) {
-                    setMalads(response.data);
-                    setFilteredCompanies(response.data);
-
-                    // Extract unique locations for filter options
-                    // const uniqueLocations = [
-                    //     ...new Set(
-                    //         response.data.map((malad) => malad?.Location)
-                    //     ),
-                    // ];
-                    // setLocations(uniqueLocations);
-                } else {
-                    setMalads([]);
-                    setFilteredCompanies([]);
-                }
+                console.log(response.data);
+                
+                setMalads(response.data.malads);
+                setFilteredMalads(response.data.malads);
             } catch (error) {
+                console.log(error);
+
                 setError("حدث خطأ أثناء تحميل بيانات المريض.");
             } finally {
                 setLoading(false);
@@ -53,22 +37,26 @@ function List() {
         };
         fetchMalads();
     }, [id, user.id]);
+
     useEffect(() => {
-        const filtered = malads.filter((company) => {
-            // const matchesType = typeFilter
-            //     ? company?.Type === typeFilter
-            //     : true;
-            // const matchesLocation = locationFilter
-            //     ? company?.Location === locationFilter
-            //     : true;
-            const matchesSearch = company?.Name.toLowerCase().includes(
-                searchQuery.toLowerCase()
-            );
+        if (malads) {
+            const filtered = malads.filter((malad) => {
+                // const matchesType = typeFilter
+                //     ? malad?.Type === typeFilter
+                //     : true;
+                // const matchesLocation = locationFilter
+                //     ? malad?.Location === locationFilter
+                //     : true;
+                if (!malad) return false;
+                const matchesSearch = malad?.firstName
+                    ?.toLowerCase()
+                    .includes(searchQuery.toLowerCase());
 
-            return matchesType && matchesLocation && matchesSearch;
-        });
+                return matchesSearch;
+            });
 
-        setFilteredCompanies(filtered);
+            setFilteredMalads(filtered);
+        }
     }, [searchQuery, malads]);
 
     if (loading) return <div>جاري التحميل...</div>;
@@ -119,23 +107,23 @@ function List() {
                 </select> */}
             </div>
 
-            {/* Companies Table */}
+            {/* Malads Table */}
             <div className="overflow-x-auto rounded-lg shadow-lg">
                 <table className="table-auto w-full text-center border border-gray-200 rounded-lg">
                     <thead>
                         <tr className="bg-gray-100 text-gray-700 font-medium">
-                            <th className="px-6 py-4">الاسم</th>
+                            <th className="px-6 py-4">البريد الإلكتروني</th>
                             <th className="px-6 py-4 border-l border-gray-200">
-                                الموقع
+                                الاسم
                             </th>
                             <th className="px-6 py-4 border-l border-gray-200">
-                                الولاية
+                                الهاتف
                             </th>
                             <th className="px-6 py-4 border-l border-gray-200">
-                                نوع المريض"
+                                العنوان
                             </th>
                             <th className="px-6 py-4 border-l border-gray-200">
-                                عدد الاطباء
+                                الجنس
                             </th>
                             <th className="px-6 py-4 border-l border-gray-200">
                                 عرض
@@ -143,31 +131,42 @@ function List() {
                         </tr>
                     </thead>
                     <tbody>
-                        {Array.isArray(filteredCompanies) &&
-                        filteredCompanies.length > 0 ? (
-                            filteredCompanies.map((company) => (
+                        {Array.isArray(filteredMalads) &&
+                        filteredMalads?.length > 0 ? (
+                            filteredMalads.map((malad) => (
                                 <tr
-                                    key={company?.id}
+                                    key={malad?.id}
                                     className="border-t border-gray-200 hover:bg-gray-50"
                                 >
-                                    <td className="px-6 py-4 font-semibold text-gray-700">
-                                        {company?.Name}
+                                    {/* Email */}
+                                    <td className="px-6 py-4 text-gray-700">
+                                        {malad?.email}
                                     </td>
+
+                                    {/* Name */}
+                                    <td className="px-6 py-4 text-gray-700">
+                                        {malad?.firstName} {malad?.lastName}
+                                    </td>
+
+                                    {/* Telephone */}
                                     <td className="px-6 py-4 text-gray-600">
-                                        {company?.Location}
+                                        {malad?.telephone || "غير متوفر"}
                                     </td>
+
+                                    {/* Address */}
                                     <td className="px-6 py-4 text-gray-600">
-                                        {company?.Wilaya}
+                                        {malad?.adress || "غير متوفر"}
                                     </td>
+
+                                    {/* Gender */}
                                     <td className="px-6 py-4 text-gray-600">
-                                        {company?.Type}
+                                        {malad?.gender || "غير متوفر"}
                                     </td>
-                                    <td className="px-6 py-4 text-gray-600">
-                                        {company?.Doctors.length}
-                                    </td>
+
+                                    {/* Action: View */}
                                     <td className="px-6 py-4">
                                         <Link
-                                            to={`/Malad/Companies/${company?.id}`}
+                                            to={`/Doctor/Malads/${malad?.id}`}
                                             className="py-1 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-200"
                                         >
                                             عرض
@@ -177,8 +176,8 @@ function List() {
                             ))
                         ) : (
                             <tr>
-                                <td colSpan="5" className="p-6 text-gray-500">
-                                    لا توجد شركات مطابقة
+                                <td colSpan="6" className="p-6 text-gray-500">
+                                    لا يوجد مرضى مطابقة
                                 </td>
                             </tr>
                         )}
